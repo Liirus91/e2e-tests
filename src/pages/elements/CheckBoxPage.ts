767821bd5d1b78969f037cfc1dd48b07123414e1
Liirus91 <work.liirus@gmail.com>
2025-12-12 07:42:30 +0200

    feat: add expand and collapse nodes buttons

--- src/pages/elements/CheckBoxPage.ts
+++ src/pages/elements/CheckBoxPage.ts
@@ -8,4 +8,18 @@ export class CheckBoxPage extends ItemPage {
       ...super.selectors,
     };
   }
+
+  async expandAllNodes(): Promise<void> {
+    const expandAllButton = await this.page.$('button[title="Expand all"]');
+    if (expandAllButton) {
+      await expandAllButton.click();
+    }
+  }
+
+  async collapseAllNodes(): Promise<void> {
+    const collapseAllButton = await this.page.$('button[title="Collapse all"]');
+    if (collapseAllButton) {
+      await collapseAllButton.click();
+    }
+  }
 }
