ef739a692f3d89b0d4ca4da7afe6458a3a67c76e
Liirus91 <work.liirus@gmail.com>
2025-12-13 07:36:45 +0200

    feat: add check box names

---
+++ src/data/submenus/elements/checkBoxNames.ts
@@ -0,0 +1,14 @@
+export const checkBoxNames = {
+  HOME: 'Home',
+  DESKTOP: 'Desktop',
+  NOTES: 'Notes',
+  COMMANDS: 'Commands',
+  DOCUMENTS: 'Documents',
+  WORKSPACE: 'Workspace',
+  OFFICE: 'Office',
+  DOWNLOADS: 'Downloads',
+  WORD_FILE: 'Word File.doc',
+  EXCEL_FILE: 'Excel File.doc',
+} as const;
+
+export type CheckBoxName = (typeof checkBoxNames)[keyof typeof checkBoxNames];
